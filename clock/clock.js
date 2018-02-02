d3b6cf49787541593fc3da59426e031fcf7856cc
procek69 <llukas.de@gmail.com>
2018-02-02 12:43:54 +0100

    Git, here I come

---
+++ clock/clock.js
@@ -0,0 +1,62 @@
+(function () {
+
+    var time = 0;
+    var timer = document.getElementById('timer');
+    var interval;
+
+    function flashing() {
+        document.getElementById('msg').innerText = 'Ostatnie zadanko';
+        timer.classList.add('blink_me');
+    }
+
+    function end() {
+        document.getElementById('msg').innerText = '';
+        timer.innerText = 'koniec';
+    }
+
+    document
+        .getElementById('button')
+        .addEventListener('click', function () {
+            var minutes = document.getElementById('time').value;
+            minutes = parseInt(minutes) || 0;
+            if (minutes < 0) {
+                alert('Knopers proszę wyjść.');
+                window.reload();
+            }
+            time = minutes * 60 * 1000;
+
+            var currentTime = new Date().getTime();
+            if (interval) clearInterval(interval);
+            interval = setInterval(function () {
+                var now = new Date().getTime();
+                var dt  = now - currentTime;
+                var left = time - dt;
+
+                if (left <= 0) {
+                    end();
+                    clearInterval(interval);
+                    end();
+                    return;
+                }
+
+                var minutes = parseInt(left / 60000);
+                left -= minutes * 60000;
+                var seconds =  parseInt(left / 1000);
+                left -= seconds * 1000;
+
+                if (minutes <= 9) {
+                    flashing();
+                }
+
+                if (minutes < 10) minutes = '0' + minutes;
+                if (seconds < 10) seconds = '0' + seconds;
+                if (left < 10) left = '00' + left;
+                else if (left < 100) left = '0' + left;
+                
+                timer.innerText = minutes + ':' + seconds + ':' + left
+            }, 10);
+    });
+    
+    
+
+})();
